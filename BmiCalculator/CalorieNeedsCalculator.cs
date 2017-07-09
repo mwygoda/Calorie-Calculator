845e97a21930c4271f149d4d9d6321b5446ac267
mw <mw@users-MBP.home>
2017-07-09 22:33:15 +0200

    better naming

---
+++ BmiCalculator/CalorieNeedsCalculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace BmiCalculator
+{
+    public class CalorieNeedsCalculator
+    {
+        public decimal CalculateCalorieNeeds(decimal bodyWeightOfPatient, string physicalActivityLevel,
+            int ageOfPatient, string genderOfPatient, decimal heightOfPatient)
+        {
+            decimal calorieNeeds = 0;
+            if (genderOfPatient == "woman")
+            {
+                if (physicalActivityLevel== "zero")
+                {
+                    return calorieNeeds = (655m + 9.6m * bodyWeightOfPatient + 1.85m* heightOfPatient - 4.7m * ageOfPatient) * 1.0m;
+                }
+                else if (physicalActivityLevel == "twoTimesWeek")
+                {
+                    return calorieNeeds = (655m + 9.6m * bodyWeightOfPatient + 1.85m* heightOfPatient - 4.7m * ageOfPatient) * 1.2m;
+                }
+                
+                else if (physicalActivityLevel == "fourTimesWeek")
+                {
+                    return calorieNeeds = (655m + 9.6m * bodyWeightOfPatient + 1.85m* heightOfPatient - 4.7m * ageOfPatient) * 1.5m;
+                }
+                else if (physicalActivityLevel == "everyday")
+                {
+                    return calorieNeeds = (655m + 9.6m * bodyWeightOfPatient + 1.85m* heightOfPatient - 4.7m * ageOfPatient) * 2.0m;
+                }
+            }
+            else
+            {
+                Console.WriteLine("man doesnt care :)");
+                return calorieNeeds = 9999999;
+            }
+            return calorieNeeds;
+        }
+    }
+}
--- BmiCalculator/Program.cs
+++ BmiCalculator/Program.cs
@@ -6,8 +6,8 @@ namespace BmiCalculator
     {
         static void Main(string[] args)
         {
-            Class1 counter = new Class1();
-            var result = counter.Calculate(90.5m, "zero", 50, "woman", 175.4m);
+            CalorieNeedsCalculator calorieCounter = new CalorieNeedsCalculator();
+            var result = calorieCounter.CalculateCalorieNeeds(90.5m, "zero", 50, "woman", 175.4m);
             Console.WriteLine(result);
         }
     }
