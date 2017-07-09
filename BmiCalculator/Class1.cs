3123cb84632d30adc1257f63ecffe2e86c58532c
mw <mw@users-MBP.home>
2017-07-09 22:07:09 +0200

    dirty Code

---
+++ BmiCalculator/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace BmiCalculator
+{
+    public class Class1
+    {
+        public decimal Calculate(decimal w, string ps, int a, string g, decimal h)
+        {
+            decimal c = 0;
+            if (g == "woman")
+            {
+                if (ps == "zero")
+                {
+                    return c = (655m + 9.6m * w + 1.85m* h - 4.7m * a) * 1.0m;
+                }
+                else if (ps == "twoTimesWeek")
+                {
+                    return c = (655m + 9.6m * w + 1.85m* h - 4.7m * a) * 1.2m;
+                }
+                
+                else if (ps == "fourTimesWeek")
+                {
+                    return c = (655m + 9.6m * w + 1.85m* h - 4.7m * a) * 1.5m;
+                }
+                else if (ps == "everyday")
+                {
+                    return c = (655m + 9.6m * w + 1.85m* h - 4.7m * a) * 2.0m;
+                }
+            }§
+            else
+            {
+                Console.WriteLine("man doesnt care :)");
+                return c = 9999999;
+            }
+            return c;
+        }
+    }
+}
---
+++ BmiCalculator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BmiCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 counter = new Class1();
+            var result = counter.Calculate(90.5m, "zero", 50, "woman", 175.4m);
+            Console.WriteLine(result);
+        }
+    }
+}/////Calculate(decimal w, string ps, int a, string g, decimal h)
