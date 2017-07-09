08377531d877a4be262f9d09ddc2e536e6184811
mw <mw@users-MBP.home>
2017-07-09 23:31:55 +0200

    Don't repeat yourself

---
+++ BmiCalculator/CalculationExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace BmiCalculator
+{
+    public static class CalculationExtensions
+    {
+        public static decimal CalculateCalorieNeeds(Patient patient,
+            decimal phisicalMultiplayer)
+        {
+            return (655m + 9.6m * patient.bodyWeight + 1.85m * patient.height - 4.7m * patient.age) * phisicalMultiplayer;
+        }
+    }
+}
--- BmiCalculator/CalorieNeedsCalculator.cs
+++ BmiCalculator/CalorieNeedsCalculator.cs
@@ -15,24 +15,16 @@ namespace BmiCalculator
                 switch (patient.physicalActivityLevel)
                 {
                     case Constants.PHISICAL_ACTIVITY_ZERO:
-                        calorieNeeds =
-                            (655m + 9.6m * patient.bodyWeight + 1.85m * patient.height - 4.7m * patient.age) *
-                            Constants.MULTIPLER_FOR_ZERO_ACTIVITY;
+                        calorieNeeds = CalculationExtensions.CalculateCalorieNeeds(patient, Constants.MULTIPLER_FOR_ZERO_ACTIVITY);
                         break;
                     case Constants.PHISICAL_ACTIVITY_LOW:
-                        calorieNeeds =
-                            (655m + 9.6m * patient.bodyWeight + 1.85m * patient.height - 4.7m * patient.age) *
-                            Constants.MULTIPLER_FOR_LOW_ACTIVITY;
+                        calorieNeeds = CalculationExtensions.CalculateCalorieNeeds(patient, Constants.MULTIPLER_FOR_LOW_ACTIVITY);
                         break;
                     case Constants.PHISICAL_ACTIVITY_MEDIUM:
-                        calorieNeeds =
-                            (655m + 9.6m * patient.bodyWeight + 1.85m * patient.height - 4.7m * patient.age) *
-                            Constants.MULTIPLER_FOR_MEDIUM_ACTIVITY;
+                        calorieNeeds = CalculationExtensions.CalculateCalorieNeeds(patient, Constants.MULTIPLER_FOR_MEDIUM_ACTIVITY);
                         break;
                     case Constants.PHISICAL_ACTIVITY_PRO:
-                        calorieNeeds =
-                            (655m + 9.6m * patient.bodyWeight + 1.85m * patient.height - 4.7m * patient.age) *
-                            Constants.MULTIPLER_FOR_PRO_ACTIVITY;
+                        calorieNeeds = CalculationExtensions.CalculateCalorieNeeds(patient, Constants.MULTIPLER_FOR_PRO_ACTIVITY);
                         break;
                     default:
                         Console.WriteLine("man doesnt care :)");
